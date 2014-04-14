ffd1a7ffcd2d0b12cb3a4f73439e9416e382e2e8
playniuniu <playniuniu@gmail.com>
2014-04-14 11:26:29 +0800

    polish customer page

--- app/static/appjs/customer.js
+++ app/static/appjs/customer.js
@@ -8,19 +8,49 @@
 $(function() {
     "use strict";
 
-    // 客户分布
-    var donut = new Morris.Donut({
-        element: 'donut-chart',
+    // 机柜分布
+    var cabinet = new Morris.Donut({
+        element: 'cabinet-chart',
         resize: true,
         colors: ["#00c0ef", "#00a65a", "#f39c12", "#f56954"],
         data: [
-            {label: "互联网", value: 72},
-            {label: "政府", value: 5},
-            {label: "中小企业", value: 8},
-            {label: "金融", value: 12}
+            {label: "蓝汛", value: 6},
+            {label: "腾讯", value: 10},
+            {label: "班班通", value: 2},
+            {label: "兴业银行", value: 1}
         ],
         hideHover: 'auto',
-        formatter: function (x) { return x + "%"}
+        formatter: function (x) { return x + " 柜"}
+    });
+
+    // 带宽分布
+    var band = new Morris.Donut({
+        element: 'band-chart',
+        resize: true,
+        colors: ["#00c0ef", "#00a65a", "#f39c12", "#f56954"],
+        data: [
+            {label: "蓝汛", value: 20},
+            {label: "腾讯", value: 20},
+            {label: "班班通", value: 2},
+            {label: "兴业银行", value: 1}
+        ],
+        hideHover: 'auto',
+        formatter: function (x) { return x + " GB"}
+    });
+
+    // IP分布
+    var ip = new Morris.Donut({
+        element: 'ip-chart',
+        resize: true,
+        colors: ["#00c0ef", "#00a65a", "#f39c12", "#f56954"],
+        data: [
+            {label: "蓝汛", value: 128},
+            {label: "腾讯", value: 64},
+            {label: "班班通", value: 8},
+            {label: "兴业银行", value: 8}
+        ],
+        hideHover: 'auto',
+        formatter: function (x) { return x + " 个"}
     });
 
 });
